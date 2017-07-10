8805fbd2ab659fe43c28d61777e5b38e80e61ec8
Theodore Ateba <tf.ateba@gmail.com>
2017-07-10 23:40:09 +0200

    Fix the drivers.mk files for I2C and SPI.

--- os/hal/ports/AVR/TINY/I2Cv1/driver.mk
+++ os/hal/ports/AVR/TINY/I2Cv1/driver.mk
@@ -1,9 +1,9 @@
 ifeq ($(USE_SMART_BUILD),yes)
 ifneq ($(findstring HAL_USE_I2C TRUE,$(HALCONF)),)
-PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/I2Cv1/hal_adc_lld.c
+PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/I2Cv1/hal_i2c_lld.c
 endif
 else
-PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/I2Cv1/hal_adc_lld.c
+PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/I2Cv1/hal_i2c_lld.c
 endif
 
 PLATFORMINC += ${CHIBIOS}/os/hal/ports/AVR/TINY/I2Cv1
--- os/hal/ports/AVR/TINY/SPIv1/driver.mk
+++ os/hal/ports/AVR/TINY/SPIv1/driver.mk
@@ -1,9 +1,9 @@
 ifeq ($(USE_SMART_BUILD),yes)
 ifneq ($(findstring HAL_USE_SPI TRUE,$(HALCONF)),)
-PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/SPIv1/hal_adc_lld.c
+PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/SPIv1/hal_spi_lld.c
 endif
 else
-PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/SPIv1/hal_adc_lld.c
+PLATFORMSRC += ${CHIBIOS}/os/hal/ports/AVR/TINY/SPIv1/hal_spi_lld.c
 endif
 
 PLATFORMINC += ${CHIBIOS}/os/hal/ports/AVR/TINY/SPIv1
